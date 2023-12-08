990876a83d63b33bc0cccd88402557d557f6b7d6
Anna Karutina <anna.karutina@voco.ee>
2023-12-08 10:00:50 +0200

    Create database

---
+++ demo_create_db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql')
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty'
+})
+
+con.connect((err) => {
+    if(err) throw err
+    console.log('MySQL Server connected')
+    let sql = 'CREATE DATABASE mydb'
+    con.query(sql, (err, result) => {
+        if(err) throw err
+        console.log('Database mydb created')
+    })
+})
