28821ad091e52d51c812f083a800af25078ffc62
Anna Karutina <anna.karutina@voco.ee>
2023-12-08 11:16:13 +0200

    Select all fields from table

---
+++ demo_db_select.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql')
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'mydb'
+})
+
+con.connect((err) => {
+    if(err) throw err
+    console.log('MySQL Server connected')
+    let sql = "SELECT * FROM customers"
+    con.query(sql, (err, result) => {
+        if(err) throw err
+        console.log(result)
+    })
+})
