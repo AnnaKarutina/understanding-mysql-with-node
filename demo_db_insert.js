175e60cad857488204dcd2a1691d8fc135b28caf
Anna Karutina <anna.karutina@voco.ee>
2023-12-08 10:40:53 +0200

    Add data into customers table - multiply rows

--- demo_db_insert.js
+++ demo_db_insert.js
@@ -10,9 +10,24 @@ const con = mysql.createConnection({
 con.connect((err) => {
     if(err) throw err
     console.log('MySQL Server connected')
-    let sql = 'INSERT INTO customers (name, address) VALUES ("Company Inc", "Highway 37")'
+    let sql = `INSERT INTO customers (name, address) VALUES
+        ('John', 'Highway 71'),
+        ('Peter', 'Lowstreet 4'),
+        ('Amy', 'Apple st 652'),
+        ('Hannah', 'Mountain 21'),
+        ('Michael', 'Valley 345'),
+        ('Sandy', 'Ocean blvd 2'),
+        ('Betty', 'Green Grass 1'),
+        ('Richard', 'Sky st 331'),
+        ('Susan', 'One way 98'),
+        ('Vicky', 'Yellow Garden 2'),
+        ('Ben', 'Park Lane 38'),
+        ('William', 'Central st 954'),
+        ('Chuck', 'Main Road 989'),
+        ('Viola', 'Sideway 1633')`;
     con.query(sql, (err, result) => {
+        console.log(result)
         if(err) throw err
-        console.log('1 record inserted')
+        console.log('Number of records inserted: ' + result.affectedRows)
     })
 })
