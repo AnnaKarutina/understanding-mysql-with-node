59693f3df6e26c2d2287d439d09f0599711d5041
Anna Karutina <anna.karutina@voco.ee>
2023-12-08 10:06:59 +0200

    Create customers table

---
+++ demo_create_table.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql')
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'mydb'
+})
+
+con.connect((err) => {
+    if(err) throw err
+    console.log('MySQL Server connected')
+    let sql = 'CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))'
+    con.query(sql, (err, result) => {
+        if(err) throw err
+        console.log('Table customers created')
+    })
+})
